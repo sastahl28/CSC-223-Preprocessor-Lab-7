ce6de3850aecf4e09277a262db2d6ba77bb6b97c
sastahl28 <112001400+sastahl28@users.noreply.github.com>
2022-11-02 15:21:42 -0400

    Added outline to preprocessor

--- CSC-223-Lab-7-Preprocessor/src/preprocessor/delegates/ImplicitPointPreprocessor.java
+++ CSC-223-Lab-7-Preprocessor/src/preprocessor/delegates/ImplicitPointPreprocessor.java
@@ -17,12 +17,24 @@ public class ImplicitPointPreprocessor
 	 * 
 	 * Algorithm:
 	 *    TODO
+	 *  1. Compute the set of implied points.
+	 *  make a list (or something) of the implied points
+	 *  	
+	 *  
+	 *	2. Use the implicit points to compute the implicit minimal segments.
+	 *
+	 *
+	 *	3. Identify the complete set of minimal segments (accounting for implicit points).
+	 *	4. Construct the set of all non-minimal segments (based on minimal segments).
+	 *	5. Populate a container all non-minimal segments.
+	 *	6. Populate a container with all segments; this is our ‘database’
+	 *    
 	 */
 	public static Set<Point> compute(PointDatabase givenPoints, List<Segment> givenSegments)
 	{
 		Set<Point> implicitPoints = new LinkedHashSet<Point>();
 
-        // TODO
+		// TODO
 
 		return implicitPoints;
 	}
