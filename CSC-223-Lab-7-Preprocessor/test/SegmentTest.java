df367dcd34ad2cbdce37504239472d9ca08af9ad
sastahl28 <112001400+sastahl28@users.noreply.github.com>
2022-11-10 21:27:21 -0500

    Almost done with segment test

--- CSC-223-Lab-7-Preprocessor/test/SegmentTest.java
+++ CSC-223-Lab-7-Preprocessor/test/SegmentTest.java
@@ -1,5 +1,9 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
 import org.junit.jupiter.api.Test;
 
 import geometry_objects.Segment;
@@ -7,7 +11,7 @@ import geometry_objects.points.Point;
 
 class SegmentTest {
 
-	//create the points in the segments
+	//create the points in the figure
 	Point _p1 = new Point(0 ,2);
 	Point _p2 = new Point(2 ,0);
 	Point _p3 = new Point(2 ,4);
@@ -17,12 +21,12 @@ class SegmentTest {
 	Point _p7 = new Point(9, 2);
 	Point _p8 = new Point(7, 0);
 	Point _p9 = new Point(10 ,9);
-	Point _p10 = new Point(10 ,7);
+	Point _p10 = new Point(10 ,6);
 	Point _p11 = new Point(7 ,5);
 	Point _p12 = new Point(8, 7);
 	Point _p13 = new Point(2 ,10);
 	Point _p14 = new Point(2 ,5);
-	Point _p15 = new Point(2,6);
+	Point _p15 = new Point(2,7);
 	Point _p16 = new Point(4 ,4);
 
 	//create the set of segments in the figure
@@ -43,6 +47,7 @@ class SegmentTest {
 
 	//used for comparing methods
 	public static final double POSITIVE_INFINITY = 1.0 / 0.0;
+
 
 
 	@Test
@@ -78,17 +83,19 @@ class SegmentTest {
 		assertEquals(4.0 , _a.length());
 		assertEquals(9.0 , _b.length());
 		assertEquals(3.0 , _c.length());
+
 		assertEquals(9.486832980505138 , _d.length());
 		assertEquals(4.47213595499958 , _e.length());
 		assertEquals(2.23606797749979, _f.length());
-		assertEquals(8.54400374531753 , _g.length());
-		assertEquals(1.0 , _i.length());
+		assertEquals(8.94427190999916 , _g.length());
+		assertEquals(2.0 , _i.length());
 
 		//check the length of a segment where the endpoints are the same
 		assertEquals( 0.0 , _k.length());
 
 		//check two segments that are the same length
 		assertEquals(_b.length() , _j.length());
+		assertEquals(_i.length(), _l.length());
 
 	}
 
@@ -105,7 +112,7 @@ class SegmentTest {
 		assertEquals( POSITIVE_INFINITY , _a.slope());
 
 		//test on other segments that are neither
-		assertEquals(-0.375 , _g.slope());
+		assertEquals(-0.5 , _g.slope());
 		assertEquals(3.0 , _d.slope());
 		assertEquals(2.0 , _f.slope());
 		assertEquals(-1.0 , _h.slope());
@@ -131,14 +138,12 @@ class SegmentTest {
 		//create the points of intersection to compare to
 		Point point1 = new Point(2 ,2);
 		Point point2 = new Point(5.5 ,2);
-		Point point3 = new Point(9.1432857142857143 , 6.428571428571429);
 
 		//find intersection of two segments
 		assertEquals(point1 ,_a.segmentIntersection(_b));
 		assertEquals(point2 , _e.segmentIntersection(_b));
 		assertEquals(point2 , _e.segmentIntersection(_c));
-		//question about this one!
-		//assertEquals( point3,_e.segmentIntersection(_g));
+
 
 		//test in segments where the endpoint falls on segment
 		assertEquals(null , _b.segmentIntersection(_l));
@@ -160,19 +165,90 @@ class SegmentTest {
 
 	@Test
 	void testPointLiesOn() {
-		fail("Not yet implemented");
+
+		//test segments and lines that fall on segment
+		assertTrue(_b.pointLiesOn(_p1));
+		assertTrue(_b.pointLiesOn(_p4));
+		assertTrue(_b.pointLiesOn(_p6));
+		assertTrue(_b.pointLiesOn(_p7));
+
+		//test points not on the segment
+		assertFalse(_b.pointLiesOn(_p8));
+		assertFalse(_b.pointLiesOn(_p10));
+		assertFalse(_b.pointLiesOn(_p9));
+		assertFalse(_b.pointLiesOn(_p14));
+
+		//test points on a segment
+		assertTrue(_g.pointLiesOn(_p10));
+		assertTrue(_g.pointLiesOn(_p12));
+		assertTrue(_g.pointLiesOn(_p13));
+
+		//test points not on the segment
+		assertFalse(_g.pointLiesOn(_p8));
+		assertFalse(_g.pointLiesOn(_p15));
+		assertFalse(_g.pointLiesOn(_p9));
+		assertFalse(_g.pointLiesOn(_p14));
+
 	}
 
 
 	@Test
 	void testPointLiesOnSegment() {
-		fail("Not yet implemented");
+		//test segments and lines that fall on segment
+		assertTrue(_b.pointLiesOnSegment(_p1));
+		assertTrue(_b.pointLiesOnSegment(_p4));
+		assertTrue(_b.pointLiesOnSegment(_p6));
+		assertTrue(_b.pointLiesOnSegment(_p7));
+
+		//test points not on the segment
+		assertFalse(_b.pointLiesOnSegment(_p8));
+		assertFalse(_b.pointLiesOnSegment(_p10));
+		assertFalse(_b.pointLiesOnSegment(_p9));
+		assertFalse(_b.pointLiesOnSegment(_p14));
+
+		//test points on a segment
+		assertTrue(_g.pointLiesOnSegment(_p10));
+		assertTrue(_g.pointLiesOnSegment(_p12));
+		assertTrue(_g.pointLiesOnSegment(_p13));
+
+		//test points not on the segment
+		assertFalse(_g.pointLiesOnSegment(_p8));
+		assertFalse(_g.pointLiesOnSegment(_p15));
+		assertFalse(_g.pointLiesOnSegment(_p9));
+		assertFalse(_g.pointLiesOnSegment(_p14));
 	}
 
 
 	@Test
 	void testPointLiesBetweenEndpoints() {
-		fail("Not yet implemented");
+		
+		//test endpoints of a segment
+		assertFalse(_b.pointLiesBetweenEndpoints(_p1));
+		assertFalse(_b.pointLiesBetweenEndpoints(_p7));
+		
+		//test on points on the segment
+		assertTrue(_b.pointLiesBetweenEndpoints(_p4));
+		assertTrue(_b.pointLiesBetweenEndpoints(_p6));
+		
+
+		//test points not on the segment
+		assertFalse(_b.pointLiesBetweenEndpoints(_p8));
+		assertFalse(_b.pointLiesBetweenEndpoints(_p10));
+		assertFalse(_b.pointLiesBetweenEndpoints(_p9));
+		assertFalse(_b.pointLiesBetweenEndpoints(_p14));
+
+		//test the endpoints of a segment
+		assertFalse(_g.pointLiesBetweenEndpoints(_p10));
+		assertFalse(_g.pointLiesBetweenEndpoints(_p13));
+		
+		//test with points on the segment
+		assertTrue(_g.pointLiesBetweenEndpoints(_p12));
+
+		//test points not on the segment
+		assertFalse(_g.pointLiesBetweenEndpoints(_p8));
+		assertFalse(_g.pointLiesBetweenEndpoints(_p15));
+		assertFalse(_g.pointLiesBetweenEndpoints(_p9));
+		assertFalse(_g.pointLiesBetweenEndpoints(_p14));
 	}
 
 	@Test
@@ -210,6 +286,7 @@ class SegmentTest {
 
 	@Test
 	void testSharedVertex() {
+
 		//test on points that do not intersect
 		assertEquals(null , _l.sharedVertex(_a));
 		assertEquals(null , _g.sharedVertex(_e));
@@ -262,12 +339,35 @@ class SegmentTest {
 
 	@Test
 	void testIsCollinearWith() {
-		fail("Not yet implemented");
+
+		//test on two segments that do not overlap
+		assertFalse(_a.isCollinearWith(_l));
+		assertFalse(_i.isCollinearWith(_d));
+
+		//test on segments that coincide without overlap
+		assertTrue(_c.isCollinearWith(_m));
+		assertTrue(_e.isCollinearWith(_f));
+
+		//test on segment that intersect
+		assertFalse(_d.isCollinearWith(_g));
+		assertFalse(_c.isCollinearWith(_e));
+		assertFalse(_b.isCollinearWith(_e));
+		assertFalse(_a.isCollinearWith(_b));
+
+		//test on segment that has one endpoint on segment
+		assertFalse(_f.isCollinearWith(_g));
+		assertFalse(_b.isCollinearWith(_l));
+
+		//test on segments that coincide but with overlap
+		assertTrue(_c.isCollinearWith(_b));
+		assertTrue(_m.isCollinearWith(_b));
+
+
 	}
 
 	@Test
 	void testHas() {
-		fail("Not yet implemented");
+
 		//test with a point not on segment
 		assertFalse(_b.has(_p9));
 		assertFalse(_b.has(_p10));
@@ -277,18 +377,29 @@ class SegmentTest {
 
 
 		//test with point on segment but not endpoint
+		assertTrue(_b.pointLiesOnSegment(_p4));
+		assertTrue(_b.pointLiesOnSegment(_p6));
 
 		//test with the endpoint
+		assertTrue(_b.pointLiesOnSegment(_p1));
+		assertTrue(_b.pointLiesOnSegment(_p7));
 
 
+		//test with a point not on segment
+		assertFalse(_g.has(_p9));
+		assertFalse(_g.has(_p1));
+		assertFalse(_g.has(_p11));
+		assertFalse(_g.has(_p5));
+		assertFalse(_g.has(_p14));
 
 		//do the same with another segment and endpoint
+		assertTrue(_g.has(_p10));
+		assertTrue(_g.has(_p13));
 	}
 
 
 	@Test
 	void testIsHorizontal() {
-
 
 		//test on segments that are horizontal 
 		assertTrue(_b.isHorizontal());
@@ -299,21 +410,17 @@ class SegmentTest {
 		assertFalse(_a.isHorizontal());
 		assertFalse(_d.isHorizontal());
 		assertFalse(_i.isHorizontal());
-		
+
 		//test on other segments
 		assertFalse(_e.isHorizontal());
 		assertFalse(_f.isHorizontal());
-		assertFalse(_g.isHorizontal());
-
-		//test on another horizontal segment
-		
+		assertFalse(_g.isHorizontal());	
 
 	}
 
 	@Test
 	void testIsVertical() {
-		
-		
+
 		//test on segments that are horizontal
 		assertTrue(_a.isVertical());
 		assertTrue(_l.isVertical());
@@ -324,34 +431,163 @@ class SegmentTest {
 		assertFalse(_c.isVertical());
 		assertFalse(_m.isVertical());
 
-		//continute to test on segments 
+		//continue to test on segments 
 		assertFalse(_e.isVertical());
 		assertFalse(_f.isVertical());
 		assertFalse(_g.isVertical());
-		
+
 	}
 
 
 
 	@Test
 	void testOther() {
-		fail("Not yet implemented");
+
+		//test with both x and y values being given
+		assertEquals( _p3 , _a.other(_p2));
+		assertEquals( _p2 , _a.other(_p3));
+		assertEquals( _p1 , _b.other(_p7));
+		assertEquals( _p7 , _b.other(_p1));
+		assertEquals( _p6 , _c.other(_p4));
+		assertEquals( _p9 , _d.other(_p8));
+		assertEquals( _p11 , _e.other(_p5));
+		assertEquals( _p12 , _f.other(_p11));
+		assertEquals( _p13 , _g.other(_p10));
+		assertEquals( _p11 , _h.other(_p13));
+		assertEquals( _p15 , _i.other(_p14));
+		assertEquals( _p7 , _j.other(_p1));
+
+
+		//test on segments that share an endpoint
+		assertEquals( _e.other(_p5) , _f.other(_p12));
+		assertEquals( _e.other(_p5) , _h.other(_p13));
+		assertEquals( _g.other(_p10) , _h.other(_p11));
+
+		//test with an endpoint not on the line
+		assertEquals( null , _d.other(_p1));
+		assertEquals( null , _h.other(_p1));
+		assertEquals( null , _m.other(_p1));
+
 	}
+
 
 	@Test
 	void testHashCode() {
-		fail("Not yet implemented");
+
+		//test on the same segment
+		assertTrue(_b.hashCode()==_j.hashCode());
+		assertEquals(_b.hashCode(), _j.hashCode());
+
+
+		//test on the same length segment
+		assertFalse(_l.hashCode()==_m.hashCode());
+		assertFalse(_f.hashCode()==_i.hashCode());
+
+
+		//segments that are the same length and same slope
+		assertTrue(_i.hashCode()==_l.hashCode());
+		assertEquals(_i.hashCode(), _l.hashCode());
+
+
+		//test on two very different segments
+		assertFalse(_g.hashCode()==_e.hashCode());
+		assertFalse(_a.hashCode()==_g.hashCode());
+		assertFalse(_e.hashCode()==_m.hashCode());
+		assertFalse(_k.hashCode()==_e.hashCode());
+		assertFalse(_c.hashCode()==_l.hashCode());
+
+
 	}
 
 
 	@Test
 	void testCoincideWtihoutOverlap() {
-		fail("Not yet implemented");
+
+		//test on two segments that do not overlap
+		assertFalse(_a.coincideWithoutOverlap(_l));
+		assertFalse(_i.coincideWithoutOverlap(_d));
+
+		//test on segments that coincide without overlap
+		assertTrue(_c.coincideWithoutOverlap(_m));
+		assertTrue(_e.coincideWithoutOverlap(_f));
+
+		//test on segment that intersect
+		assertFalse(_d.coincideWithoutOverlap(_g));
+		assertFalse(_c.coincideWithoutOverlap(_e));
+		assertFalse(_b.coincideWithoutOverlap(_e));
+		assertFalse(_a.coincideWithoutOverlap(_b));
+
+		//test on segment that has one endpoint on segment
+		assertFalse(_f.coincideWithoutOverlap(_g));
+		assertFalse(_b.coincideWithoutOverlap(_l));
+
+		//test on segments that coincide but with overlap
+		assertFalse(_c.coincideWithoutOverlap(_b));
+		assertFalse(_m.coincideWithoutOverlap(_b));
+
+
 	}
 
 	@Test
 	void testCollectOrderedPointsOnSegment() {
-		fail("Not yet implemented");
+
+		//create a set of points
+		Set<Point> allPoints = new TreeSet<Point>();
+
+		//add points to the set
+		allPoints.add(_p1);
+		allPoints.add(_p2);
+		allPoints.add(_p3);
+		allPoints.add(_p4);
+		allPoints.add(_p5);
+		allPoints.add(_p6);
+		allPoints.add(_p7);
+		allPoints.add(_p8);
+		allPoints.add(_p9);
+		allPoints.add(_p10);
+
+		//create the set of expected points
+		SortedSet<Point> expected = new TreeSet<Point>();
+
+		//add the expected points to the expected set
+		expected.add(_p1);
+		expected.add(_p6);
+		expected.add(_p4);
+		expected.add(_p7);
+
+		//ensure expected is correct
+		assertEquals(expected , _b.collectOrderedPointsOnSegment(allPoints));
+
+		assertTrue(expected.equals(_b.collectOrderedPointsOnSegment(allPoints)));
+
+		//add unexpected elements to the expected set
+		expected.add(_p10);
+
+		//test the outcome
+		_b.collectOrderedPointsOnSegment(allPoints);
+
+		assertFalse(expected.equals(_b.collectOrderedPointsOnSegment(allPoints)));
+
+		//clear set of expected and test on another segment
+		expected.clear();
+
+		//add more points to all points
+		allPoints.add(_p11);
+		allPoints.add(_p12);
+		allPoints.add(_p13);
+
+
+		//add expected elements to the set of expected elements
+		expected.add(_p10);
+		expected.add(_p12);
+		expected.add(_p13);
+
+		//test that the outcome is equal
+		assertEquals(expected , _g.collectOrderedPointsOnSegment(allPoints));
+
+		assertTrue(expected.equals(_g.collectOrderedPointsOnSegment(allPoints)));
+
+
 	}
 
 
